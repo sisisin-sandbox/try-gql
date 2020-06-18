79ac562ace1f8f0396225ee7baa24e3b30fa56d8
sisisin <azsisisin@gmail.com>
2020-06-19 00:19:40 +0900

    configure apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { useQuery, ApolloProvider } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { client } from './services/client';
+
+const REPO = gql`
+  query($number_of_repos: Int!) {
+    viewer {
+      name
+      repositories(last: $number_of_repos) {
+        nodes {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const Q: React.FC = () => {
+  const { loading, error, data } = useQuery(REPO, {
+    variables: {
+      number_of_repos: 3,
+    },
+  });
+  return <pre>{JSON.stringify(data, null, '  ')}</pre>;
+};
 
 export const App: React.FC = () => {
-  return <>hello</>;
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <Q></Q>
+      </div>
+    </ApolloProvider>
+  );
 };
