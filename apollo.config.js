13c8e4d11cfb180b01ecdf42d538e115bcde9cde
sisisin <azsisisin@gmail.com>
2020-06-19 10:33:56 +0900

    configure codegen

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'github_localfile',
+      localSchemaFile: './schema.graphql',
+    },
+  },
+};
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
+import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
-import { useQuery, ApolloProvider } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
 import { client } from './services/client';
-
-const REPO = gql`
-  query($number_of_repos: Int!) {
-    viewer {
-      name
-      repositories(last: $number_of_repos) {
-        nodes {
-          name
-        }
-      }
-    }
-  }
-`;
-
-const Q: React.FC = () => {
-  const { loading, error, data } = useQuery(REPO, {
-    variables: {
-      number_of_repos: 3,
-    },
-  });
-  return <pre>{JSON.stringify(data, null, '  ')}</pre>;
-};
+import { Repos } from './components/Repos';
 
 export const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
-        <Q></Q>
-      </div>
+      <Repos></Repos>
     </ApolloProvider>
   );
 };
