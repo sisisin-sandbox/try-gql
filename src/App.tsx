d68b964c27bf944c9483b270d8e580c715a6a545
sisisin <azsisisin@gmail.com>
2020-06-21 16:36:44 +0900

    configure router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
 import { client } from './services/client';
 import { Repos } from './components/Repos';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
 export const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <Repos></Repos>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Repos></Repos>}></Route>
+        </Routes>
+      </Router>
     </ApolloProvider>
   );
 };
