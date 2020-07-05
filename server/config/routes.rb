e45c5ad9e4ab1bd76433a954ec197ac6f15102af
sisisin <azsisisin@gmail.com>
2020-07-05 23:10:00 +0900

    rubocop

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  post '/graphql', to: 'graphql#execute'
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope '/sample' do
